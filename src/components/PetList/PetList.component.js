edba3578b881927f5a8effdb39db1d5c3238416c
Martin Di Palma <tincho.dipalma@gmail.com>
2019-08-07 20:19:42 -0300

    useState

--- src/components/PetList/PetList.component.js
+++ src/components/PetList/PetList.component.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './PetList.css';
 
 const PetList = ({ history, pets, selectPetAction }) => {
+  const [ isOpen, setIsOpen ] = useState(false);
+
   const onSelectPet = (pet) => () => {
     selectPetAction(pet);
     history.push('/pet-data');
   };
 
+  const toggleList = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const className = `PetList PetList--${isOpen ? 'open' : 'closed'}`;
   return (
-    <div className="PetList">
+    <div className={className}>
       <ul>
         {pets.map((pet, i) => (
           <li className="PetList__Item" key={i} onClick={onSelectPet(pet)}>
@@ -15,6 +24,8 @@ const PetList = ({ history, pets, selectPetAction }) => {
           </li>
         ))}
       </ul>
+
+      <button onClick={toggleList}>{isOpen ? 'Mostrar menos' : 'Mostrar más'}</button>
     </div>
   );  
 };
